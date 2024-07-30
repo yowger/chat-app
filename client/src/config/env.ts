d84f38a7fece56ef2b43181f3a711c100b924042
Roger Pantil <rogerpantilyowger@gmail.com>
2024-07-31 02:06:56 +0800

    updated config

--- client/src/config/env.ts
+++ client/src/config/env.ts
@@ -1,1 +1,4 @@
-export const API_URL = import.meta.env.VITE_SERVER_URL
+export const API_URL =
+    import.meta.env.VITE_VERCEL_ENV === "production"
+        ? import.meta.env.VITE_VERCEL_SERVER_URL
+        : import.meta.env.VITE_SERVER_URL
